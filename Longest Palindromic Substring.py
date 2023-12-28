c8b157c4c943741f583d808cbe01d3fd5940a9b1
amanjnc <110789353+amanjnc@users.noreply.github.com>
2023-12-28 17:32:09 +0300

    Update Longest Palindromic Substring.py

--- Longest Palindromic Substring.py
+++ Longest Palindromic Substring.py
@@ -1,1 +1,24 @@
-Longest Palindromic Substring
+class Solution(object):
+    def longestPalindrome(self, s):
+      
+
+        def innerStringEval(toLeft, toRight):
+            while  toLeft >=0 and toRight<len(s) and s[toRight]==s[toLeft] :
+                
+                    toRight+=1
+                    toLeft-=1
+                
+            return s[toLeft+1:toRight]
+        
+        result = ""
+        for i in range(len(s)):
+            evenPalindromeSubString=innerStringEval(i,i+1)
+
+            oddPalindromeSubString= innerStringEval(i,i)
+            largesestStringforThatindex = max(oddPalindromeSubString , evenPalindromeSubString , key=len)
+            if len(largesestStringforThatindex) > len(result):
+                result = largesestStringforThatindex
+        return result
+    
+
+
